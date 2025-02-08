9b243173ce9f108d48fa30214f7bd9813aa53517
Steven G. Johnson <stevenj@alum.mit.edu>
2025-02-08 16:53:35 -0500

    docstrings

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,74 @@
 ##############################################################
 
+"""
+    TupleVec{T<:Union{Tuple,NamedTuple}}
+    TupleVec(tuple)
+    TupleVec(args...)
+    TupleVec(; kwargs...)
+
+A `TupleVec{T}` is a wrapper around a `Tuple` or `NamedTuple` object (of type `T`)
+that can be treated as an element of an (abstract) vector space defined by the
+[direct sum](https://en.wikipedia.org/wiki/Direct_sum) of the tuple components.
+
+That is, a `TupleVec(a, b, …)` acts like a vector where `TupleVec(a, b, …) ± TupleVec(c, d, …)`
+gives `TupleVec(a±c, b±c, …)` assuming the individual components `(a, b, …)` support `+` and `-`,
+and similarly `TupleVec(a, b, …) * number` gives `TupleVec(a * number, b * number, …)`.
+
+Furthermore, if the individual elements support an inner product (`dot` methods) and/or
+a norm (`norm` methods), then the `TupleVec` does as well.  The inner product
+`TupleVec(a, b, …) ⋅ TupleVec(c, d, …)` gives `a⋅c + b⋅c + …`, and `norm(TupleVec(a, b, …))`
+correspondingly gives `sqrt(norm(a)^2 + norm(b)^2 + …)`.   The adjoint `v'`, or `adjoint(v)`,
+is an element of the [dual space](https://en.wikipedia.org/wiki/Dual_space) (the generalization
+of a "row vector"): it is a linear operator such that `v' * w == dot(v, w)`.
+
+By implementing these methods, a `TupleVec` can potentially be used to pass combinations of
+vectors to any function that only requires such abstract vector-space properties, e.g.
+for numerical integration, differentiation, or differential equations.
+
+Iterating over a `TupleVec` corresponds to iterating over each of the components in sequence,
+similar to `Iterators.flatten((a, b, …))`.   `map(f, TupleVec(a, b, …))` applies `f` recursively
+to the elements, returning a new tuple vector `TupleVec(map(f, a), map(f, b), …)`.
+
+To get back the original `tuple` from `v = TupleVec(tuple)`, you can call `Tuple(v)`.  If
+`tuple` is a `NamedTuple`, call `NamedTuple(v)` to get the named tuple back; alternatively,
+for named tuples, you can access fields `tuple.field` directly as `v.field`.
+
+Example:
+```jldoctest
+julia> v = TupleVec(1, [2,3,4], [5 6; 7 8]) # tuple of a scalar, a `Vector`, and a `Matrix`
+TupleVec(1, [2, 3, 4], [5 6; 7 8])
+
+julia> 2v
+TupleVec(2, [4, 6, 8], [10 12; 14 16])
+
+julia> v - v
+TupleVec(0, [0, 0, 0], [0 0; 0 0])
+
+julia> collect(v)
+8-element Vector{Int64}:
+ 1
+ 2
+ 3
+ 4
+ 5
+ 7
+ 6
+ 8
+
+julia> using LinearAlgebra
+
+julia> norm(v)^2, v ⋅ v, v' * v # three ways to get norm²
+(204.00000000000003, 204, 204)
+
+julia> v = TupleVec(scalar = 1, vector = [2,3,4], matrix = [5 6; 7 8]) # named tuple
+TupleVec(scalar = 1, vector = [2, 3, 4], matrix = [5 6; 7 8])
+
+julia> v.matrix
+2×2 Matrix{Int64}:
+ 5  6
+ 7  8
+```
+"""
 struct TupleVec{T<:Union{Tuple,NamedTuple}}
     t::T
 end
